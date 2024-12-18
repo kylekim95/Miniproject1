f56380aa8c76a9e59a065fc3021a27faba81c245
soyoung125 <sypark041754@gmail.com>
2024-12-18 16:37:34 +0900

    refactor: handle empty content

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 // main.js
-import breadcrumb from "./breadcrumbs.js";
 import mainHeader from "./mainHeader.js";
 import axiosInstance from "./axiosInstance.js";
 
@@ -11,21 +10,27 @@ async function render(path = "", query) {
     data = await response.data;
   } catch (error) {}
 
-  if (response.status === 200 && data?.id > 0) {
-    return await body(data);
-  } else {
-   const response = await axiosInstance.get('/documents');
-   const data = await response.data;
-   const newId = Number(data[data.length - 1].id) + 1;
-   return await body({id:newId, title:"새로운 글 제목", content:"내용을 입력 바랍니다."});
-  }
+  return await body(data);
+
+  // if (response.status === 200 && data?.id > 0) {
+  //   return await body(data);
+  // } else {
+  //   const response = await axiosInstance.get("/documents");
+  //   const data = await response.data;
+  //   const newId = Number(data[data.length - 1].id) + 1;
+  //   return await body({
+  //     id: newId,
+  //     title: "새로운 글 제목",
+  //     content: "내용을 입력 바랍니다.",
+  //   });
+  // }
 }
 
 async function body(data) {
   let header = await mainHeader(data);
   let pre_main = `  
   <div class="flex-shrink-1" >
-    <div id="did" class="d-none">${data.id}</div>`
+    <div id="did" class="d-none">${data.id}</div>`;
   let post_main = `
   <div class="m-3 mt-5 ps-5 pe-5" style="overflow-y: auto; height: 100%;"  >
       <!-- 제목 -->
@@ -36,13 +41,13 @@ async function body(data) {
       </div>
       <!-- 컨텐츠 -->
       <div>
-        <blockquote id="contents"  class="p-1 fw-medium" contenteditable="true">
-          ${data.content}
+        <blockquote id="contents" class="p-1 fw-medium" contenteditable="true">
+          ${data.content ? data.content : ""}
         </blockquote>
       </div>
       </div>
     </div>
-  </div>`
+  </div>`;
   return pre_main + header + post_main;
 }
 
