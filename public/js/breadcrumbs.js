06baadda3d764e8bbfb96e1c0dbcafc689fbd5a9
Sang Yup Kim <kylekim95@gmail.com>
2024-12-18 10:17:01 +0900

    feat 경로 생략 기능 추가
    경로 길이 가 maxPathSize 이상이면 앞, 뒤 2개씩 제외하고 "..."으로 생략

--- public/js/breadcrumbs.js
+++ public/js/breadcrumbs.js
@@ -1,3 +1,5 @@
+const maxPathSize = 4;
+
 function FindPath(current, target) {
   if(current.id == target) return [ current ];
   if(!current.documents) return [];
@@ -23,12 +25,32 @@ async function render(data) {
   let header = `<nav aria-label="breadcrumb"> <ol class="breadcrumb breadcrumb-chevron p-3 rounded-3">`;
   let footer = `</ol> </nav>`;
   let content = ``;
-  traverse.slice(0, -1).forEach((e)=>{
-    content += `<li class="breadcrumb-item">
-                  <div class="pe-auto" onclick="location='${(e.id === -1 ? '/' : '/app/'+e.id)}'">${e.title}</div>
-                </li>`;
-  });
+  console.log(traverse.length);
+  switch(true){
+    case traverse.length <= maxPathSize:
+      traverse.slice(0, -1).forEach((e)=>{
+        content += `<li class="breadcrumb-item">
+                      <div class="pe-auto" onclick="location='${(e.id === -1 ? '/' : '/app/'+e.id)}'">${(e.title.length > 10 ? e.title.slice(0, 10) : e.title)}</div>
+                    </li>`;
+      });
+      break;
+    case traverse.length > maxPathSize:
+      traverse.slice(0, 2).forEach((e)=>{
+        content += `<li class="breadcrumb-item">
+                      <div class="pe-auto" onclick="location='${(e.id === -1 ? '/' : '/app/'+e.id)}'">${(e.title.length > 10 ? e.title.slice(0, 10) : e.title)}</div>
+                    </li>`;
+      });
+      content += `<li class="breadcrumb-item">...</li>`;
+      traverse.slice(-2, -1).forEach((e)=>{
+        content += `<li class="breadcrumb-item">
+                      <div class="pe-auto" onclick="location='${(e.id === -1 ? '/' : '/app/'+e.id)}'">${(e.title.length > 10 ? e.title.slice(0, 10) : e.title)}</div>
+                    </li>`;
+      });
+      break;
+  }
   content += `<li class="breadcrumb-item active fw-bold" aria-current="page">${traverse.at(-1).title}</li>`;
+
+
   return header + content + footer;
 }
 
