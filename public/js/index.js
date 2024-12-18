b59df5ee4e11d6f6d600e1f64de652ebb3da8aed
Sang Yup Kim <kylekim95@gmail.com>
2024-12-18 20:01:39 +0900

    feat breadcrumb SPA

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import event from "./event.js"
 import main from "./main.js"
 import welcome from "./welcome.js"
 import navi from "./navi.js"
+import ErrorPage from "./ErrorPage.js";
 
 const $navi = document.getElementById('navi');
 const $content = document.getElementById('content');
@@ -25,15 +26,22 @@ const render = async (path = "", query= "") => {
     const rootPath = "/" + paths[1];
     const component = routes.find(route => rootPath === route.path)?.component;
     if(component == null){
-      $content.innerHTML = "Not found!!"
+      $content.innerHTML = await ErrorPage(path, query);
     }else{
       $content.innerHTML = await component(path, query);
     }
   } catch (err) {
-     $content.innerHTML = "Not found!!"
+    $content.innerHTML = await ErrorPage(path, query);
     console.error(err);
   }
 };
+
+const route = () => {
+  const path = window.location;
+  const pathname = path.pathname;
+  render(pathname, "");
+}
+window.route = route;
 
 render($path, $query);
 event();
