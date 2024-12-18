c88bd25527c5401c7a5b7071f1188c03a4a87446
jaeyo03 <toyazx@naver.com>
2024-12-18 17:31:59 +0900

    feat : autosave 기능 구현

--- public/js/event.js
+++ public/js/event.js
@@ -1,8 +1,8 @@
 // event.js
+import axiosInstance from "./axiosInstance.js";
 
 const event = () => {
   window.addEventListener("click", (e) => {
-    console.log(e.target);
     if (e.target.matches("#save")) {
       saveEvent();
     } else if (e.target.matches("#write") || e.target.matches("#write1")) {
@@ -44,101 +44,45 @@ function navExpand() {
   navExpand.classList.remove("d-block");
 }
 
-async function saveEvent() {
-  const newid = document.getElementById("did").innerHTML.trim();
-  const title = document.getElementById("title").innerHTML.trim();
-  const contents = document.getElementById("contents").innerHTML.trim();
-  const replaceContents = contents
-    .replaceAll("<div>", "")
-    .replaceAll("</div>", "<br>");
-
-  const response = await fetch(`http://localhost:3001/document/${newid}`);
-  let data;
-  try {
-    data = await response.json();
-  } catch (error) {}
-
-  if (response.ok && data?.id > 0) {
-    // update
-    const response = await fetch(`http://localhost:3001/document/${newid}`, {
-      method: "PUT",
-      // headers: {"Content-Type": "application/json",},
-      body: JSON.stringify({
-        id: newid,
-        title: title,
-        content: replaceContents,
-      }),
-    });
-
-    if (response.ok) {
-      alert("업데이트 성공!");
-      location.href = "http://localhost:3000/app/" + newid;
-    } else {
-      alert("업데이트에 실패하였습니다.");
-    }
-  } else {
-    // post
-    const response = await fetch(`http://localhost:3001/document/`, {
-      method: "POST",
-      // headers:{"Content-Type": "application/json"},
-      body: JSON.stringify({
-        id: newid,
-        title: title,
-        content: replaceContents,
-      }),
-    });
-    if (response.ok) {
-      alert("등록 성공!");
-      location.href = "http://localhost:3000/app/" + newid;
-    } else {
-      alert("등록에 실패하였습니다.");
-    }
-  }
-}
-
 async function autoSaveEvent() {
-  const savingUI = document.getElementById("savingStatus");
+  const savingUI = document.getElementById('savingStatus');
 
   // UI에 "저장 중" 표시
-  savingUI.classList.remove("d-none");
+  savingUI.classList.remove('d-none');
 
-  async function saving(id, obj) {
-    try {
-      console.log("데이터 업데이트");
-      // await axios.put(`http://localhost:3001/document/${id}`, obj);
-    } catch (err) {
-      if (err.response && err.response.status === 404) {
-        // ID 없으면 post 요청
-        // await axios.post(`http://localhost:3001/document`, obj);
-        console.log("새 데이터 저장");
-      } else {
-        console.error("저장 실패:", err);
-      }
-    } finally {
-      // UI에서 "저장 중" 표시 제거
-      setTimeout(() => {
-        savingUI.classList.add("d-none");
-      }, 600);
+  const currentId = document.getElementById("did").innerHTML.trim();
+  const title = document.getElementById("title").innerHTML.trim();
+  const contents = document.getElementById("contents").innerHTML.trim();
+  const replaceContents = contents.replaceAll("<div>","").replaceAll("</div>","<br>");
+  const jsonData = JSON.stringify({id:currentId,title:title,content:replaceContents});
+  try{
+    const response = await axiosInstance.put(`/documents/${currentId}`,jsonData);
+    if(response.data.username !== 'sajotuna'){
+      const tmp = JSON.stringify({...jsonData,id: currentId + 103});
     }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // UI에서 "저장 중" 표시 제거
+    setTimeout(() => {
+      savingUI.classList.add('d-none');
+    }, 600);
   }
-  saving();
 }
 
 function writeEvent() {
   window.addNewNote(null);
 }
 
-async function deleteEvent() {
+async function deleteEvent(){
   const deleteId = document.getElementById("did").innerHTML.trim();
+  const response = await axiosInstance.delete(`/documents/${deleteId}`);
+  const data = response.data;
 
-  const response = await fetch(`http://localhost:3001/document/${deleteId}`, {
-    method: "DELETE",
-  });
-
-  if (response.ok) {
+  if (response.status === 200 && data?.id > 0) {
     alert("삭제 되었습니다.");
     location.href = "http://localhost:3000/app/";
-  } else {
+  }else{
     alert("삭제에 실패하였습니다.");
   }
 }
